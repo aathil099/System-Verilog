fbeb9b8963c3e65a3aac96026dcf822ca612d0c5
aathil <aathilahmeth@gmail.com>
2025-02-05 19:39:36 +0530

    Refactor n-adder module for improved carry handling and update testbench timing

--- n_adder.sv
+++ n_adder.sv
@@ -1,14 +1,17 @@
 module n_adder #(parameter N = 8) (
-    input logic signed [N-1:0] A,B,
-    input logic ci,
+    input  logic signed [N-1:0] A, B,
+    input  logic ci,
     output logic signed [N-1:0] S,
     output logic co
 );
-    logic C [N-1:0]; // Carry bits
-    assign C[0] = ci; // First carry bit is the carry-in bit
-    
+    logic [N:0] C; // Carry bits
+
+    assign C[0] = ci;  // First carry bit (carry-in)
+    assign co = C[N];  // Final carry-out
+
     genvar i;
-        for (i = 0; i < N; i = i + 1) begin: adder
+    generate 
+        for (i = 0; i < N; i = i + 1) begin : adder
             full_adder fa (
                 .a   (A[i]),
                 .b   (B[i]),
@@ -17,4 +20,5 @@ module n_adder #(parameter N = 8) (
                 .sum (S[i])
             );
         end
+    endgenerate
 endmodule
--- n_adder_tb.sv
+++ n_adder_tb.sv
@@ -1,5 +1,5 @@
 module n_adder_tb ;
-    timeunit 10ns; timeprecision 1ps;
+    timeunit 1ns; timeprecision 1ps;
 
     localparam N = 8;
     
@@ -9,14 +9,15 @@ module n_adder_tb ;
     n_adder #(.N(N)) dut(.*); // Instantiate the DUT
     
     initial begin
-        $dumpfile("dump.vcd"); $dumpvars(0,dut);
+        $dumpfile("dump.vcd"); 
+        $dumpvars(0,dut);
         
         A <= 8'd5; B <= 8'd10; ci <= 0; // A = 5, B = 10, ci = 0
         #1 assert (S == 8'd15) else $error("Incorrect");
 
         #10 A <= 8'd30; B <= -8'd10; ci <= 0;
-        #10 A <= 8'd5; B <= -8'd10; ci <= 1;
-        #10 A <= 8'd127; B <= -8'd1; ci <= 0;
+        #10 A <= 8'd5; B <= 8'd10; ci <= 1;
+        #10 A <= 8'd127; B <= 8'd1; ci <= 0;
         
         repeat (10) begin
             #9
